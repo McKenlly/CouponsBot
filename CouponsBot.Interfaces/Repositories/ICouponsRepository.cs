dc97462b941c962203fac4957faba70ca7e89b2a
Sergey Bokoch <bokoch98@yandex.ru>
2021-03-23 00:58:15 +0300

    add logic

---
+++ CouponsBot.Interfaces/Repositories/ICouponsRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using CouponsBot.Domain.Models;
+using Hazzik.Maybe;
+
+namespace CouponsBot.Interfaces.Repositories
+{
+    public interface ICouponsRepository
+    {
+        public void AddOrUpdate(Coupon coupon);
+        public void AddOrUpdate(IReadOnlyCollection<Coupon> coupons);
+        public void RemoveCoupon(Coupon coupon);
+        public void RemoveCouponById(int id);
+        public Maybe<Coupon> FindCouponById(int id);
+        public bool IsExistByCouponById(int id);
+        public bool IsExistByCoupon(Coupon coupon);
+    }
+}
