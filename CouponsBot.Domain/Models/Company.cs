a5df88041c14245494484f3afe59de70df65be21
Sergey Bokoch <bokoch98@yandex.ru>
2021-03-27 01:34:26 +0300

    модельки. Только с печки

---
+++ CouponsBot.Domain/Models/Company.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CouponsBot.Domain.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string IconUrl { get; set; }
+        public IReadOnlyCollection<Coupon> Coupons { get; set; }
+    }
+}
---
+++ CouponsBot.Domain/Models/Rating.cs
@@ -0,0 +1,12 @@
+﻿namespace CouponsBot.Domain.Models
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int CouponId { get; set; }
+        public Coupon Coupon { get; set; }
+        public int Mark { get; set; }
+    }
+}
