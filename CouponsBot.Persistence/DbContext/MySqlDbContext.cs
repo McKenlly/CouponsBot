ef6c31c012987abb29510e904b58f3167e9475cd
Sergey Bokoch <bokoch98@yandex.ru>
2021-03-27 13:45:51 +0300

    контекст

---
+++ CouponsBot.Persistence/DbContext/MySqlDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+using CouponsBot.Domain.Models;
+using CouponsBot.Persistence.Configurations;
+
+namespace CouponsBot.Persistence.DbContext
+{
+    public class MySqlDbContext : System.Data.Entity.DbContext
+    {
+        public Microsoft.EntityFrameworkCore.DbSet<Coupon> Coupons { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<Rating> Ratings { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<User> Users { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<Company> Companies { get; set; }
+        
+        public MySqlDbContext() : base("CoouponsDb") {}
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.AddFromAssembly(typeof(CompanyEntityTypeConfiguration).Assembly);
+        }
+
+        protected override DbEntityValidationResult ValidateEntity(DbEntityEntry entityEntry, IDictionary<object, object> items)
+        {
+            return base.ValidateEntity(entityEntry, items);
+        }
+    }
+}
